dd5d249efc9fbdb14ce4661255b1ad00ec782391
ReadTheWind <liuhuanasjob@163.com>
2018-07-24 10:16:53 +0800

    新增监听器

--- src/main/java/com/log/logservice/LogserviceApplication.java
+++ src/main/java/com/log/logservice/LogserviceApplication.java
@@ -2,12 +2,14 @@ package com.log.logservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author liuhuan
  * 日志服务启动类
  */
 @SpringBootApplication
+@ServletComponentScan
 public class LogserviceApplication {
 
 	public static void main(String[] args) {
